4bc1f5bb3f6f160fb3990121586890abd82783d1
Indika Jayasinghe <indika@brightroll.com>
2012-03-08 01:06:51 -0800

    Added a layout based on Bootstrap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= bootstrap
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def is_current_controller(cntlr_name)
+    params[:controller] == cntlr_name ?  ' class="active"' : ''
+  end
 end
