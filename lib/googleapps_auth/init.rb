3e112180cf85bf0a723b881ae4c4a8f62e0d23c1
Aaron Stone <aaron@serendipity.cx>
2012-03-14 01:10:40 -0700

    Since the gem is a gem, it doesn't need to be in lib/

--- lib/googleapps_auth/init.rb
+++
@@ -1,7 +0,0 @@
-config.gem "ruby-openid", :version => "2.1.8", :lib => "openid"
-
-require 'googleapps_auth'
-
-config.after_initialize do
-  ActionController::Base.send :include, GoogleAppsAuth
-end
--- lib/googleapps_auth/lib/googleapps_auth/railtie.rb
+++
@@ -1,7 +0,0 @@
-module GoogleAppsAuth
-  class Railtie < ::Rails::Railtie
-    config.after_initialize do
-      ActionController::Base.send :include, GoogleAppsAuth
-    end
-  end
-end
