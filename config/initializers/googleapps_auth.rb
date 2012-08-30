6d397887d985dee3c9b3a8da42c04306d70eae8d
Aaron Stone <aaron@brightroll.com>
2012-08-29 18:02:59 -0700

    Put the cert bundle into the repo.

--- config/initializers/googleapps_auth.rb
+++ config/initializers/googleapps_auth.rb
@@ -1,2 +1,2 @@
-GoogleAppsAuth.certificate_authority_file = "/opt/local/etc/openssl/cert.pem"
+GoogleAppsAuth.certificate_authority_file = "config/curl-ca-bundle.crt"
 GoogleAppsAuth.default_domain = "brightroll.com"
