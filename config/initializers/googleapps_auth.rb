764f0e161cae2d437381127db8c0f544714b6737
Aaron Stone <aaron@brightroll.com>
2014-01-06 15:30:43 -0800

    Google Apps Auth commentary

--- config/initializers/googleapps_auth.rb
+++ config/initializers/googleapps_auth.rb
@@ -1,2 +1,7 @@
+# You can use your system's CA bundle or provide a private bundle.
+# Since this app is often deployed on OS X, which uses Keychain to store certs,
+# a local copy of Curl's public CA bundle is included.
 GoogleAppsAuth.certificate_authority_file = "config/curl-ca-bundle.crt"
-GoogleAppsAuth.default_domain = "brightroll.com"
+
+# Set this to your company's domain to restrict logins
+# GoogleAppsAuth.default_domain = "example.com"
